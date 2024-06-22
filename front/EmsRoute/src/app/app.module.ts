c5c841056ccb2a62dc62e8c56404a2408f9be73f
speranos <aoueldma@student.1337.ma>
2024-06-22 18:40:45 +0100

    add record

--- front/EmsRoute/src/app/app.module.ts
+++ front/EmsRoute/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserGridComponent } from './user-grid/user-grid.component';
+import { AddComponent } from './add/add.component';
 // import { EditComponent } from './edit/edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserGridComponent,
+    AddComponent,
   ],
   imports: [
     BrowserModule,
