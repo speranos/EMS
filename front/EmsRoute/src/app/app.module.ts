71f7cb09aaa197646310f75ae5c4e580d49cb7d8
speranos <aoueldma@student.1337.ma>
2024-06-22 14:08:46 +0100

    front set up

---
+++ front/EmsRoute/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserGridComponent } from './user-grid/user-grid.component';
+// import { EditComponent } from './edit/edit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserGridComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ front/EmsRoute/src/app/user-grid/user-grid.component.ts
@@ -0,0 +1,16 @@
+// src/app/user-grid/user-grid.component.ts
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'user-grid',
+  templateUrl: './user-grid.component.html',
+  styleUrls: ['./user-grid.component.css']
+})
+export class UserGridComponent implements OnInit {
+  constructor() { }
+
+
+
+  ngOnInit(): void {
+  }
+}
